629399721adf0c7375f24afd9835868ad60997b7
Mariusz Lisowski <59373615+mariuszlisowski@users.noreply.github.com>
2020-08-19 20:40:31 +0100

    Game class (#1)
    * Add CMake/GTest config files
    
    * Add Game class header
    
    * Add Game class source
    
    * Add main empty entry point
    
    * Add GTest - Game class test
    
    * Git ignore list

---
+++ bowling/src/game.cpp
@@ -0,0 +1,54 @@
+#include "game.hpp"
+
+void Game::roll(int pins) {
+    rolls[currentRoll++] = pins;
+}
+
+int Game::score() {
+    int score_{};
+    int firstFrameThrow{};
+    for (int frame = 0; frame < MAX_FRAMES; ++frame) {
+        if (isStrike(firstFrameThrow)) {
+            score_ += MAX_FRAME_SCORE +
+                      nextTwoBallsForStrike(firstFrameThrow);
+            firstFrameThrow++;
+        } else if (isSpare(firstFrameThrow)) {
+            score_ += MAX_FRAME_SCORE +
+                      nextBallForSpare(firstFrameThrow);
+            firstFrameThrow += 2;
+        } else {
+            score_ += twoBallsInFrame(firstFrameThrow);
+            firstFrameThrow += 2;
+        }
+    }
+    return score_;
+}
+
+void Game::reset() {
+    currentRoll = 0;
+}
+
+bool Game::isSpare(int firstFrameThrow) {
+    return (rolls[firstFrameThrow] +
+            rolls[++firstFrameThrow] ==
+            MAX_PINS);
+}
+
+bool Game::isStrike(int firstFrameThrow) {
+    return (rolls[firstFrameThrow] ==
+            MAX_PINS);
+}
+
+int Game::nextTwoBallsForStrike(int firstFrameThrow) {
+    return rolls[++firstFrameThrow] +
+           rolls[++firstFrameThrow];
+}
+
+int Game::nextBallForSpare(int firstFrameThrow) {
+    return rolls[firstFrameThrow + 2];
+}
+
+int Game::twoBallsInFrame(int firstFrameThrow) {
+    return rolls[firstFrameThrow] +
+           rolls[++firstFrameThrow];
+}
