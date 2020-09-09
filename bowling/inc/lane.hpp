577a34fc7e5634ccb845bb3867e496a211806973
m-kop <m.kopertowski@epicspace.eu>
2020-09-09 09:41:59 +0200

    add index check for players_

--- bowling/inc/lane.hpp
+++ bowling/inc/lane.hpp
@@ -8,7 +8,12 @@ public:
     Lane(const std::string& name) : name_(name) {}
 
     size_t getPlayersNum() const { return players_.size(); }
-    std::string getPlayer(size_t index) const { return players_.at(index); }
+    std::string getPlayer(size_t index) const {
+        if (index < players_.size()) {
+            return players_[index];
+        }
+        return "";
+    }
     std::string getName() const { return name_; }
 
     void addPlayer(const std::string& player) { players_.push_back(player); }
